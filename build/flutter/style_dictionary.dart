42ce0a8cf4c7bb996dcc5eb3eb26bd35801f8f4a
zzunebye <bhang96@gmail.com>
2022-09-14 17:17:39 +0900

    generated flutter code

---
+++ build/flutter/style_dictionary.dart
@@ -0,0 +1,47 @@
+
+//
+// style_dictionary.dart
+//
+
+// Do not edit directly
+// Generated on Wed, 14 Sep 2022 07:09:12 GMT
+
+
+
+import 'dart:ui';
+
+class StyleDictionary {
+  StyleDictionary._();
+
+    static const aliasBorderRadiusButton = 8;
+    static const aliasColorFillGreen = #00ff00;
+    static const aliasColorFillPrimaryRed = #FF2257;
+    static const aliasColorTextWhite = #ffffff;
+    static const aliasSizingSizeL = 44;
+    static const aliasSpacingSpaceL = 16;
+    static const aliasSpacingXxxxl = 40;
+    static const aliasTextCaseTitle = capitalize;
+    static const aliasTypographyBodyRegular = [object Object];
+    static const componentBorderRadiusButton = 8;
+    static const componentColorButton2BackgroundColor = #00ff00;
+    static const componentColorButtonBackgroundColor = #FF2257;
+    static const componentColorButtonTextColor = #ffffff;
+    static const componentSizingButtonL = 44;
+    static const componentSpacingBodyLastBtnPadding = 40;
+    static const componentSpacingBodyPagePaddingLr = 16;
+    static const componentTextCaseTitle = capitalize;
+    static const componentTypographyButtonL = [object Object];
+    static const option14 = 14;
+    static const option16 = 16;
+    static const option18 = 18;
+    static const option40 = 40;
+    static const option44 = 44;
+    static const option8 = 8;
+    static const optionGreen = #00ff00;
+    static const optionInter = Inter;
+    static const optionLetterspaceM = -0.1;
+    static const optionRed500 = #FF2257;
+    static const optionRegular = Regular;
+    static const optionTitle = capitalize;
+    static const optionWhite = #ffffff;
+}
